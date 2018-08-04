5c7b46a862a545fe49ae56465df6091d0409468c
Julius Šakalys <julius.sakalys@gmail.com>
2018-08-04 17:15:54 +0700

    Added analytics (and disabled unused service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import ReactGA from  'react-ga';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-79716516-2');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
